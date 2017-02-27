559f7d15a814cda87e633b423ad4b1625a20df60
Esperanza Moreno Cruz <hola@mi-mina.com>
2017-02-27 22:12:30 +0100

    index

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
-/* global $ */
+/* global d3 */
 
 // var date = new Date(Date.parse("2016-12-23"))
 // console.log(date)
 
-$.ajax({
-  url: './data/index.json',
-  type: 'GET',
-  dataType: 'json',
-  success: printJson
-})
+d3.json('./data/index.json', function (error, data) {
+  if (error) throw error
 
-function printJson (data) {
-  console.log(data)
-}
+  var items = data.elements
+
+  d3.select('#items')
+    .selectAll('.items')
+    .data(items)
+    .enter()
+    .append('div')
+    .attr('class', '.items col-xs-12 col-sm-6 col-md-4 col-lg-3')
+    .append('a')
+    .attr('href', function (d) { return d.href })
+    .append('img')
+    .attr('class', 'img-responsive center-block cuadricula')
+    .attr('alt', function (d) { return d.title })
+    .attr('src', function (d) { return d.img })
+
+  var songs = items.filter(function (d) {
+    return d.tag === 'songs'
+  })
+
+  console.log(songs)
+})
