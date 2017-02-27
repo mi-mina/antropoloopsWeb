5ddade996146d6fb8ebbd071df659115c514895d
Esperanza Moreno Cruz <hola@mi-mina.com>
2017-02-27 20:54:41 +0100

    index

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log(new Date)
+/* global $ */
+
+// var date = new Date(Date.parse("2016-12-23"))
+// console.log(date)
+
+$.ajax({
+  url: './data/index.json',
+  type: 'GET',
+  dataType: 'json',
+  success: printJson
+})
+
+function printJson (data) {
+  console.log(data)
+}
