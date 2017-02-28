8720a26e91d801afa1c66c97c03d01fec75a60cb
Esperanza Moreno Cruz <hola@mi-mina.com>
2017-02-28 13:19:28 +0100

    pages

--- js/pages.js
+++ js/pages.js
@@ -3,10 +3,16 @@
 // var date = new Date(Date.parse("2016-12-23"))
 // console.log(date)
 
-d3.json('data/index.json', function (error, data) {
-  if (error) throw error
+var filterBy = d3.select('#content')
+  .attr('class')
 
-  var items = data.elements.reverse()
+d3.json('../data/index.json', function (error, data) {
+  if (error) throw error
+  console.log(filterBy)
+
+  var items = data.elements.filter(function (d) {
+    return d.tag === filterBy
+  }).reverse()
 
   d3.select('#items')
     .selectAll('.items')
@@ -15,9 +21,9 @@ d3.json('data/index.json', function (error, data) {
     .append('div')
     .attr('class', '.items col-xs-12 col-sm-6 col-md-4 col-lg-3')
     .append('a')
-    .attr('href', function (d) { return d.href })
+    .attr('href', function (d) { return '../' + d.href })
     .append('img')
     .attr('class', 'img-responsive center-block cuadricula')
     .attr('alt', function (d) { return d.title })
-    .attr('src', function (d) { return d.img })
+    .attr('src', function (d) { return '../' + d.img })
 })
