7ec281337fc4f7888f059a249a5e8b91752bfaa2
Esperanza Moreno Cruz <hola@mi-mina.com>
2017-03-01 20:50:01 +0100

    add transparent color with title to items when hover

--- js/main.js
+++ js/main.js
@@ -1,23 +1,58 @@
 /* global d3 */
 
-// var date = new Date(Date.parse("2016-12-23"))
+// var date = new Date(Date.parse('2016-12-23'))
 // console.log(date)
 
 d3.json('data/index.json', function (error, data) {
   if (error) throw error
 
-  var items = data.elements.reverse()
+  var itemsArray = data.elements.reverse()
 
-  d3.select('#items')
+  var items = d3.select('#items')
     .selectAll('.items')
-    .data(items)
+    .data(itemsArray)
     .enter()
     .append('div')
-    .attr('class', '.items col-xs-12 col-sm-6 col-md-4 col-lg-3')
+    .attr('class', 'items col-xs-12 col-sm-6 col-md-4 col-lg-3')
+
+  var hrefs = items
     .append('a')
     .attr('href', function (d) { return d.href })
+
+  hrefs
     .append('img')
     .attr('class', 'img-responsive center-block cuadricula')
     .attr('alt', function (d) { return d.title })
     .attr('src', function (d) { return d.img })
+
+  var info = hrefs
+    .append('div')
+    .attr('class', 'imgTitle')
+
+  info
+    .append('p')
+    .attr('class', 'tag')
+    .text(function (d) { return d.tag })
+  info
+    .append('p')
+    .attr('class', 'title')
+    .text(function (d) { return d.title })
+  info
+    .append('p')
+    .attr('class', 'date')
+    .text(function (d) { return formatDate(d.date) })
 })
+
+function formatDate (dateString) {
+  var monthNames = [ 'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'
+]
+
+  var date = new Date(dateString)
+  var day = date.getDate()
+  var monthIndex = date.getMonth()
+  var year = date.getFullYear()
+  console.log(day + ' ' + monthNames[monthIndex] + ' ' + year)
+
+  return day + ' ' + monthNames[monthIndex] + ' ' + year
+}
