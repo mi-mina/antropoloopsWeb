7a3abfe2a55ab4e2141a022546e47c3f5e1064d7
Esperanza Moreno Cruz <hola@mi-mina.com>
2017-03-06 19:25:34 +0100

    add hover over songs, albums and news

--- js/pages.js
+++ js/pages.js
@@ -3,24 +3,61 @@
 var filterBy = d3.select('#content')
   .attr('class')
 
+console.log(filterBy)
+
 d3.json('../data/index.json', function (error, data) {
   if (error) throw error
-  console.log(filterBy)
 
-  var items = data.elements.filter(function (d) {
+  var filteredData = data.elements.filter(function (d) {
     return d.tag === filterBy
   }).reverse()
 
-  d3.select('#items')
+  var items = d3.select('#items')
     .selectAll('.items')
-    .data(items)
+    .data(filteredData)
     .enter()
     .append('div')
-    .attr('class', '.items col-xs-12 col-sm-6 col-md-4 col-lg-3')
+    .attr('class', 'items col-xs-12 col-sm-6 col-md-4 col-lg-3')
+
+  var hrefs = items
     .append('a')
     .attr('href', function (d) { return '../' + d.href })
+
+  hrefs
     .append('img')
     .attr('class', 'img-responsive center-block cuadricula')
     .attr('alt', function (d) { return d.title })
     .attr('src', function (d) { return '../' + d.img })
+
+  var info = hrefs
+    .append('div')
+    .attr('class', 'imgTitle')
+
+  info
+    .append('p')
+
+  info
+    .append('p')
+    .attr('class', 'tag')
+    .text(function (d) { return d.tag })
+  info
+    .append('p')
+    .attr('class', 'title')
+    .text(function (d) { return d.title })
+  info
+    .append('p')
+    .attr('class', 'date')
+    .text(function (d) { return formatDate(d.date) })
 })
+
+function formatDate (dateString) {
+  var monthNames = [ 'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December']
+
+  var date = new Date(dateString)
+  var day = date.getDate()
+  var monthIndex = date.getMonth()
+  var year = date.getFullYear()
+
+  return day + ' ' + monthNames[monthIndex] + ' ' + year
+}
